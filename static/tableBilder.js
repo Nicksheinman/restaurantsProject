8e3b63ef66d5e200017596ecbdb0bd6a50f1cc25
Nick <nicksheinman@gmail.com>
2024-04-15 17:53:21 +0300

    table bilder1

--- static/tableBilder.js
+++ static/tableBilder.js
@@ -1,13 +1,32 @@
 var table = document.querySelector('.table');
+var move=false
 function setTable(){
-    document.onmousemove = (event) => {
-        var x = event.clientX*100/window.innerWidth+'%';
-        var y = event.clientY*100/window.innerHeight+'%';
-        
-            table.style.transition = "0s";
-            table.style.left = x;
-            table.style.top = y;
-            table.style.transform='translate'
-        console.log(x)
+    if (move==false) {
+        document.onmousemove = (event) => {
+            var x = event.clientX*100/window.innerWidth+'%';
+            var y = event.clientY*100/window.innerHeight+'%';
+                table.style.transition = "0s";
+                table.style.left = x;
+                table.style.top = y;
+                table.style.transform='translate';
+            window['move']=true;
+    }}
+    else  {
+        document.onmousemove= (event) => {
+            table.style.transform='none'
+        }
+        window['move']=false;
+    }
+}
+
+function createTable(num){
+    if (num==1){
+        let t=document.createElement('div')
+        t.className='table1'
+        let  b=document.createElement('button')
+        b.innerText='hello'
+        b.addEventListener('click', setTable)
+        document.getElementById("main").appendChild(t)
+        document.getElementById("main").appendChild(b)
     }
 }
